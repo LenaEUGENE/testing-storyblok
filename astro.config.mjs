78ce436b95f8995eab78f429ef3f6bf6088694d0
Lena EUGENE <lenaeugene@gmail.com>
2023-02-23 08:54:16 +0100

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: "ueFL8s94LQKaHJg1D3Wa8wtt",
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+        hero: "storyblok/Hero",
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
